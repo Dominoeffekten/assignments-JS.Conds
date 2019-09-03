3c2f190b1e8674bf933f0524b99e54f9f8017332
Dominoeffekten <31511825+Dominoeffekten@users.noreply.github.com>
2019-09-03 12:40:26 +0200

    Add files via upload

---
+++ jsConds0.js
@@ -0,0 +1,177 @@
+'use strict'; 
+
+let roll1 = 0;
+let roll2 = 0;
+let roll3 = 0;
+let roll4 = 0;
+let roll5 = 0;
+let roll6 = 0;
+let flip;
+
+//1
+flip = Math.random();
+flip = Math.floor( flip * 6 + 1 );
+if ( flip === 1 )
+    roll1++;
+else if ( flip === 2 )
+    roll2++;
+else if ( flip === 3 )
+    roll3++;
+else if ( flip === 4 )
+    roll4++;
+else if ( flip === 5 )
+    roll5++;
+else 
+    roll6++;
+
+//2
+flip = Math.random();
+flip = Math.floor( flip * 6 + 1 );
+if ( flip === 1 )
+    roll1++;
+else if ( flip === 2 )
+    roll2++;
+else if ( flip === 3 )
+    roll3++;
+else if ( flip === 4 )
+    roll4++;
+else if ( flip === 5 )
+    roll5++;
+else 
+    roll6++;
+
+//3
+flip = Math.random();
+flip = Math.floor( flip * 6 + 1 );
+if ( flip === 1 )
+    roll1++;
+else if ( flip === 2 )
+    roll2++;
+else if ( flip === 3 )
+    roll3++;
+else if ( flip === 4 )
+    roll4++;
+else if ( flip === 5 )
+    roll5++;
+else 
+    roll6++;
+
+//4
+flip = Math.random();
+flip = Math.floor( flip * 6 + 1 );
+if ( flip === 1 )
+    roll1++;
+else if ( flip === 2 )
+    roll2++;
+else if ( flip === 3 )
+    roll3++;
+else if ( flip === 4 )
+    roll4++;
+else if ( flip === 5 )
+    roll5++;
+else 
+    roll6++;
+
+//5
+flip = Math.random();
+flip = Math.floor( flip * 6 + 1 );
+if ( flip === 1 )
+    roll1++;
+else if ( flip === 2 )
+    roll2++;
+else if ( flip === 3 )
+    roll3++;
+else if ( flip === 4 )
+    roll4++;
+else if ( flip === 5 )
+    roll5++;
+else 
+    roll6++;
+
+//6
+flip = Math.random();
+flip = Math.floor( flip * 6 + 1 );
+if ( flip === 1 )
+    roll1++;
+else if ( flip === 2 )
+    roll2++;
+else if ( flip === 3 )
+    roll3++;
+else if ( flip === 4 )
+    roll4++;
+else if ( flip === 5 )
+    roll5++;
+else 
+    roll6++;
+
+//7
+flip = Math.random();
+flip = Math.floor( flip * 6 + 1 );
+if ( flip === 1 )
+    roll1++;
+else if ( flip === 2 )
+    roll2++;
+else if ( flip === 3 )
+    roll3++;
+else if ( flip === 4 )
+    roll4++;
+else if ( flip === 5 )
+    roll5++;
+else 
+    roll6++;
+
+//8
+flip = Math.random();
+flip = Math.floor( flip * 6 + 1 );
+if ( flip === 1 )
+    roll1++;
+else if ( flip === 2 )
+    roll2++;
+else if ( flip === 3 )
+    roll3++;
+else if ( flip === 4 )
+    roll4++;
+else if ( flip === 5 )
+    roll5++;
+else 
+    roll6++;
+
+//9
+flip = Math.random();
+flip = Math.floor( flip * 6 + 1 );
+if ( flip === 1 )
+    roll1++;
+else if ( flip === 2 )
+    roll2++;
+else if ( flip === 3 )
+    roll3++;
+else if ( flip === 4 )
+    roll4++;
+else if ( flip === 5 )
+    roll5++;
+else 
+    roll6++;
+
+//10
+flip = Math.random();
+flip = Math.floor( flip * 6 + 1 );
+if ( flip === 1 )
+    roll1++;
+else if ( flip === 2 )
+    roll2++;
+else if ( flip === 3 )
+    roll3++;
+else if ( flip === 4 )
+    roll4++;
+else if ( flip === 5 )
+    roll5++;
+else 
+    roll6++;
+
+
+console.log('Terningen landede på 1: ' + roll1);
+console.log('Terningen landede på 2: ' + roll2);
+console.log('Terningen landede på 3: ' + roll3);
+console.log('Terningen landede på 4: ' + roll4);
+console.log('Terningen landede på 5: ' + roll5);
+console.log('Terningen landede på 6: ' + roll6);
